344eba67bffd7a4fb908992ede8052a908d888cd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-30 12:05:54 -0400

    test: Add more tests and improve a test in test_intersection.

--- intersection/tests/test_intersection.py
+++ intersection/tests/test_intersection.py
@@ -21,4 +21,18 @@ def test_intersection_list_single_double():
     first_list = [1, 2, 3, 4, 5]
     second_list = [4, 5, 6, 7, 8]
     intersection_single = main.compute_intersection_list_single(first_list, second_list)
+    intersection_double = main.compute_intersection_list_double(first_list, second_list)
     assert len(intersection_single) == 2
+    assert len(intersection_double) == 2
+    assert intersection_single == intersection_double
+
+
+def test_intersection_tuple_single_double():
+    """Ensure that intersection works for both of the list algorithms."""
+    first_tuple = (1, 2, 3, 4, 5)
+    second_tuple = (4, 5, 6, 7, 8)
+    intersection_single = main.compute_intersection_tuple_single(first_tuple, second_tuple)
+    intersection_double = main.compute_intersection_tuple_double(first_tuple, second_tuple)
+    assert len(intersection_single) == 2
+    assert len(intersection_double) == 2
+    assert intersection_single == intersection_double
