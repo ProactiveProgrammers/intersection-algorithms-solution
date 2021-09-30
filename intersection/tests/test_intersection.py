4410aaf4e8abf6a5a34593229eb98fdc98cedb15
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-30 12:02:47 -0400

    fix: Add new test cases and make fix to test_intersection.

--- intersection/tests/test_intersection.py
+++ intersection/tests/test_intersection.py
@@ -1,6 +1,6 @@
 """Test suite to ensure that each function words correctly."""
 
-from intersection  import __version__
+from intersection import __version__
 
 from intersection import main
 
@@ -8,3 +8,17 @@ from intersection import main
 def test_version():
     """Confirm that the version of the program is correct."""
     assert __version__ == "0.1.0"
+
+
+def test_generate_random_container_list():
+    """Ensure that generation of random container is correct size."""
+    random_container = main.generate_random_container(10, 100, False)
+    assert len(random_container) == 10
+
+
+def test_intersection_list_single_double():
+    """Ensure that intersection works for both of the list algorithms."""
+    first_list = [1, 2, 3, 4, 5]
+    second_list = [4, 5, 6, 7, 8]
+    intersection_single = main.compute_intersection_list_single(first_list, second_list)
+    assert len(intersection_single) == 2
